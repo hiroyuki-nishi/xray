bc20ce00d7dd05771e09fd8bd8956dd024220c93
西 悠之 <hiroyuki.nishi@motex.co.jp>
2020-10-11 16:15:10 +0900

    一旦コミット

---
+++ modules/usecase/src/test/scala/example/account/CreateAccountUseCaseTest.scala
@@ -0,0 +1,17 @@
+package example.account
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.diagrams.Diagrams
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.collection.immutable.List
+
+class CreateAccountUseCaseTest extends AnyWordSpec with Diagrams with MockFactory {
+  "CreateAccountUseCaseTest" when {
+    "test" should {
+      "x" in {
+        assert(true)
+      }
+    }
+  }
+}
